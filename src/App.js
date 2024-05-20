410b908367335e437658f92dd8d4114f017d3e4e
Hari Narayan <harinarayan947255@gmail.com>
2024-05-20 21:30:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>CSS Grid</h1>
+    <div className="containner">
+      
+      <div className="box box-1">1</div>
+      <div className="box box-2">2</div>
+      <div className="box box-3">3</div>
+      <div className="box box-4">4</div>
+      <div className="box box-5">5</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
